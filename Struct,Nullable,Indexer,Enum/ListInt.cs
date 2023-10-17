ef531dd629346d8e210d6131a7bee239fb3953a6
asmann18 <asimanjg@code.edu.az>
2023-10-18 03:35:02 +0400

    task is done

---
+++ Struct,Nullable,Indexer,Enum/ListInt.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace Struct_Nullable_Indexer_Enum;
+
+public class ListInt
+{
+    int[] numbers = new int[0];
+    public int this[int i]
+    {
+        get
+        {
+            return numbers[i];
+        }
+        set
+        {
+            numbers[i] = value;
+        }
+    }
+    public void Add(int num)
+    {
+        Array.Resize(ref numbers, numbers.Length+1);
+        numbers[^1] = num;
+    }
+    public void AddRange(params int[] nums)
+    {
+        foreach (int num in nums)
+        {
+            Add(num);
+        }
+    }
+    public bool Contains(int num)
+    {
+        foreach (int number in numbers)
+        {
+            if (number == num)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Sum()
+    {
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"Sum: {sum}");
+    }
+    public void Remove(int num)
+    {
+        if (!Contains(num))
+        {
+            Console.WriteLine($"{num} not found");
+            
+        }
+        else
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == num)
+                {
+                    for(int j = i; j < numbers.Length - 1; j++)
+                    {
+                        numbers[j] = numbers[j + 1];
+                    }
+                    Array.Resize(ref numbers, numbers.Length - 1);
+                    Console.WriteLine("number is deleted");
+                    break;
+                }
+            }
+        }
+    }
+    public void RemoveRange(params int[] nums)
+    {
+        foreach (int num in nums)
+        {
+            Remove(num);
+        }
+    }
+    public override string ToString()
+    {
+        StringBuilder builder= new StringBuilder();
+        foreach (int num in numbers)
+        {
+            builder.Append(num+",");
+        }
+        return builder.ToString();
+    }
+}
+
---
+++ Struct,Nullable,Indexer,Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Struct_Nullable_Indexer_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt listInt = new ListInt();
+            listInt.Add(1);
+            listInt.Add(2);
+            listInt.Add(3);
+            listInt.AddRange(3, 4, 6);
+            listInt.Remove(6);
+            listInt.RemoveRange(2, 3, 5);
+            Console.WriteLine(listInt[1]);
+            listInt[1] = 999;
+
+            Console.WriteLine(listInt);
+        }
+    }
+}
